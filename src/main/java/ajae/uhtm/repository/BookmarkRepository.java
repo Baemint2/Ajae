7e1f2df4d143b320c0eda1bf6db61bd028028f17
Baemint2 <qofndlwl@gmail.com>
2024-10-31 02:50:28 +0900

    [fix] 스테이징 이슈 커밋

--- src/main/java/ajae/uhtm/repository/BookmarkRepository.java
+++
@@ -1,7 +0,0 @@
-package ajae.uhtm.repository;
-
-import ajae.uhtm.entity.Bookmark;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookmarkRepository extends JpaRepository<Bookmark, Long>, QueryBookmarkRepository {
-}
